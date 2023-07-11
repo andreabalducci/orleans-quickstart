e70b7bd47f25b46a4c6baeebb5a62684fd73d7f8
Andrea Balducci <mtb.snowboard@gmail.com>
2023-07-11 17:14:16 +0200

    producer

--- src/Standalone/Silo/Services/Generators/BackgroundGeneratorGrain.cs
+++ src/Standalone/Silo/Services/Generators/BackgroundGeneratorGrain.cs
@@ -1,5 +1,6 @@
 using Orleans.Runtime;
 using Orleans.Services;
+using Standalone.Silo.Shared;
 
 namespace Standalone.Silo.Services.Generators;
 
@@ -9,16 +10,20 @@ public interface IBackgroundGeneratorGrain : IGrainService
 
 public class BackgroundGeneratorGrain : GrainService, IBackgroundGeneratorGrain
 {
+    private readonly IGrainFactory _grainFactory;
     private readonly ILogger<BackgroundGeneratorGrain> _logger;
+    private readonly IClusterClient _client;
 
     public BackgroundGeneratorGrain(
         IServiceProvider services,
         GrainId id,
         Orleans.Runtime.Silo silo,
         ILoggerFactory loggerFactory,
-        IGrainFactory grainFactory)
+        IGrainFactory grainFactory, IClusterClient client)
         : base(id, silo, loggerFactory)
     {
+        _grainFactory = grainFactory;
+        _client = client;
         _logger = loggerFactory.CreateLogger<BackgroundGeneratorGrain>();
     }
 
@@ -27,15 +32,40 @@ public class BackgroundGeneratorGrain : GrainService, IBackgroundGeneratorGrain
         await base.StartInBackground();
         _logger.LogWarning("Starting timer");
 
-        RegisterTimer(Tick, null,
+        RegisterTimer(Tick, "channel-1",
             TimeSpan.FromSeconds(5),
             TimeSpan.FromSeconds(5)
         );
     }
 
-    private Task Tick(object arg)
+    private async Task Tick(object arg)
     {
-        _logger.LogWarning("Tick");
-        return Task.CompletedTask;
+        var grain = _grainFactory.GetGrain<IStreamProducerGrain>(arg as string);
+        await grain.Produce(DateTime.UtcNow.ToString("O"));
+    }
+}
+
+public interface IStreamProducerGrain : IGrainWithStringKey
+{
+    ValueTask Produce(string value);
+}
+
+public class StreamProducerGrain : Grain, IStreamProducerGrain
+{
+    private readonly ILogger<StreamProducerGrain> _logger;
+
+    public StreamProducerGrain(ILogger<StreamProducerGrain> logger)
+    {
+        _logger = logger;
+    }
+
+    public async ValueTask Produce(string value)
+    {
+        _logger.LogInformation("Produce {Value}", value);
+       
+        var provider = this.GetStreamProvider(Constants.STREAM_PROVIDER_NAME);
+        var streamId = StreamId.Create("RANDOMDATA", this.GetPrimaryKeyString());
+        var stream = provider.GetStream<int>(streamId);
+        await stream.OnNextAsync(DateTime.Now.Millisecond);
     }
 }
